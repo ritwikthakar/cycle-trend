8f28f6fe0e6e60a1f372e3cfedc51841f8fa8a90
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2022-09-11 16:13:48 -0400

    Update combination_algos.py

--- combination_algos.py
+++ combination_algos.py
@@ -9,53 +9,13 @@ import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
 import mplfinance as mpf
-from plotly.subplots import make_subplots
+import talib
 import streamlit as st
 ticker = st.sidebar.text_input('Enter Ticker', 'SPY')
 t = st.sidebar.selectbox('Select Number of Days', ('1y', '1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'))
 i = st.sidebar.selectbox('Select Time Granularity', ('1d', '1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'))
 
-import requests
-import os
-import sys
-import subprocess
 
-# check if the library folder already exists, to avoid building everytime you load the pahe
-if not os.path.isdir("/tmp/ta-lib"):
-
-    # Download ta-lib to disk
-    with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
-        response = requests.get(
-            "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
-        )
-        file.write(response.content)
-    # get our current dir, to configure it back again. Just house keeping
-    default_cwd = os.getcwd()
-    os.chdir("/tmp")
-    # untar
-    os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
-    os.chdir("/tmp/ta-lib")
-    os.system("ls -la /app/equity/")
-    # build
-    os.system("./configure --prefix=/home/appuser")
-    os.system("make")
-    # install
-    os.system("make install")
-    # back to the cwd
-    os.chdir(default_cwd)
-    sys.stdout.flush()
-
-# add the library to our current environment
-from ctypes import *
-
-lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
-# import library
-try:
-    import talib
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib"])
-finally:
-    import talib
 
 
 # In[2]:
