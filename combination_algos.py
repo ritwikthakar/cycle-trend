bc26ae2dafb70ee6b39166487917c4bb827c7be1
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2022-09-16 17:19:06 -0400

    Update combination_algos.py

--- combination_algos.py
+++ combination_algos.py
@@ -52,8 +52,8 @@
 # pip install Ta-Lib
 
 url = 'https://launchpad.net/~mario-mariomedina/+archive/ubuntu/talib/+files'
-wget $url/libta-lib0_0.4.0-oneiric1_amd64.deb -qO libta.deb
-wget $url/ta-lib0-dev_0.4.0-oneiric1_amd64.deb -qO ta.deb
+wget url/libta-lib0_0.4.0-oneiric1_amd64.deb -qO libta.deb
+wget url/ta-lib0-dev_0.4.0-oneiric1_amd64.deb -qO ta.deb
 dpkg -i libta.deb ta.deb
 pip install ta-lib
 
